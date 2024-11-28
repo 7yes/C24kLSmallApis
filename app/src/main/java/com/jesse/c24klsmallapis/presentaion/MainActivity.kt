221e91c847800be85df9a358019e7a396f3f5a20
yesmc <7yesid@gmail.com>
2024-11-28 08:58:34 -0500

    complete with details

--- app/src/main/java/com/jesse/c24klsmallapis/presentaion/MainActivity.kt
+++ app/src/main/java/com/jesse/c24klsmallapis/presentaion/MainActivity.kt
@@ -4,20 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.jesse.c24klsmallapis.domain.model.SmModel
 import com.jesse.c24klsmallapis.presentaion.uistate.UIState
 import com.jesse.c24klsmallapis.ui.theme.C24kLSmallApisTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,41 +54,98 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun InitScreen() {
-    
+
     val navController = rememberNavController()
-    NavHost( navController = navController, startDestination = "home" ){
-        composable("home"){
-            HomeScreen()
+    lateinit var itemSlected: SmModel
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen() {
+                navController.navigate("detail")
+                itemSlected = it
+            }
         }
-        composable("detail"){
-            DetailScreen()
+        composable("detail") {
+            DetailScreen(itemSlected)
         }
     }
 }
 
 @Composable
-fun HomeScreen(viemModel: SMViewModel = hiltViewModel()) {
+fun HomeScreen(viemModel: SMViewModel = hiltViewModel(), onclikListener: (SmModel) -> Unit) {
     val state = viemModel.uiState.collectAsState()
 
-    when(state.value){
+    when (state.value) {
         is UIState.Error -> ErrorScreen((state.value as UIState.Error).error)
         UIState.Loading -> {
-            Box (Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-               CircularProgressIndicator(modifier = Modifier.size(64.dp))
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator(modifier = Modifier.size(64.dp))
             }
         }
-        is UIState.Success -> {}
+
+        is UIState.Success -> {
+            val list = (state.value as UIState.Success).mySuccessList
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(list) { item ->
+                    ItemView(item, onclikListener)
+                }
+            }
+        }
     }
 }
 
 @Composable
 fun ErrorScreen(error: String) {
-    Box (Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = error)
     }
 }
 
 @Composable
-fun DetailScreen() {
+fun DetailScreen(item: SmModel) {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        AsyncImage(
+            model = item.image, modifier = Modifier.fillMaxWidth().height(450.dp).padding(24.dp),
+            contentDescription = "Image", alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds
+        )
+        MyText("Name: ${item.name.toString()}",Color.Blue,Color.White)
+        MyText("Age: ${item.age.toString()}",Color.Blue,Color.White)
+        MyText("First Episode: ${item.firstEpisode.toString()}",Color.Blue,Color.White)
+    }
+}
 
+@Composable
+fun ItemView(item: SmModel, onClickListener: (SmModel) -> Unit) {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .padding(24.dp)
+            .clip(RoundedCornerShape(24))
+            .border(2.dp, Color.Green, shape = RoundedCornerShape(0, 24, 0, 24))
+            .fillMaxWidth()
+            .clickable { onClickListener(item) }
+            .height(250.dp)
+    ) {
+        AsyncImage(
+            model = item.image, modifier = Modifier.fillMaxWidth(),
+            contentDescription = "Image", alignment = Alignment.Center,
+            contentScale = ContentScale.FillBounds
+        )
+        MyText(item.name.toString())
+    }
+}
+
+@Composable
+fun MyText(
+    text: String = "",
+    textColor: Color = Color.White,
+    backgroundColor: Color = Color.Black
+) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor.copy(alpha = 0.5f)),
+        color = textColor, textAlign = TextAlign.Center, maxLines = 1,
+        text = text, fontSize = 26.sp
+    )
 }
